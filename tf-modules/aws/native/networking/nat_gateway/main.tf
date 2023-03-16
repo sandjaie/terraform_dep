f3d5678414a78ac0424494687890c10c04bc7d75
sandjaie <sandjaie@gmail.com>
2023-03-16 14:28:52 +0530

    networking: route tables, igw, nat added

--- tf-modules/aws/native/networking/nat_gateway/main.tf
+++ tf-modules/aws/native/networking/nat_gateway/main.tf
@@ -4,6 +4,6 @@ resource "aws_nat_gateway" "nat_gw" {
   subnet_id     = element(var.subnet_ids, count.index)
 
   tags = merge(local.tags, {
-    Name = "${var.environment}-${var.groupprefix}-nat-gateway-${element(var.availability_zones, count.index)}-${count.index + 1}"
+    Name = "${var.environment}-${var.groupprefix}-nat"
   })
 }
