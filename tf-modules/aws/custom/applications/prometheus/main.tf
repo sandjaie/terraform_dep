faf97467ba8e40ab55ed1961a04310893ff825f6
sandjaie <sandjaie@gmail.com>
2023-03-17 19:10:00 +0530

    prom test

--- tf-modules/aws/custom/applications/prometheus/main.tf
+++ tf-modules/aws/custom/applications/prometheus/main.tf
@@ -1,15 +1,15 @@
 resource "kubernetes_manifest" "configmap" {
-  manifest = yamldecode(file("${path.module}/files/configmap.yml"))
+  manifest = yamldecode(file("${path.module}/../files/configmap.yml"))
 }
 
 resource "kubernetes_manifest" "deployment" {
-  manifest = yamldecode(file("${path.module}/files/deployment.yml"))
+  manifest = yamldecode(file("${path.module}/../files/deployment.yml"))
 }
 
 resource "kubernetes_manifest" "pvc" {
-  manifest = yamldecode(file("${path.module}/files/pvc.yml"))
+  manifest = yamldecode(file("${path.module}/../files/pvc.yml"))
 }
 
 resource "kubernetes_manifest" "service" {
-  manifest = yamldecode(file("${path.module}/files/service.yml"))
+  manifest = yamldecode(file("${path.module}/../files/service.yml"))
 }
